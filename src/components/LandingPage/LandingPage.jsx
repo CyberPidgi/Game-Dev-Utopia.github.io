8ecc4711367f3f6efae6a9e7fe91cbb85d032c80
Bhavyam <bhavyamsanghavi@gmail.com>
2024-06-04 00:12:45 +0530

    UI fixed for homepage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -14,18 +14,18 @@ const LandingPage = () => {
     const [description, setDescription] = useState(null);
     const getLinks = async () => {
         try {
-            const response = await getRequest("/api/hero");
-            const video = response[0].bgVideo;
-            const diamondImages = response[0].diamondTilesImgs;
-            const img1 = diamondImages[0];
-            const img2 = diamondImages[1];
-            const img3 = diamondImages[2];
-            const desc = response[0].description;
-            setImage1(img1);
-            setImage2(img2);
-            setImage3(img3);
-            setVideoPath(video);
-            setDescription(desc);
+            // const response = await getRequest("/api/hero");
+            // const video = response[0].bgVideo;
+            // const diamondImages = response[0].diamondTilesImgs;
+            // const img1 = diamondImages[0];
+            // const img2 = diamondImages[1];
+            // const img3 = diamondImages[2];
+            // const desc = response[0].description;
+            // setImage1(img1);
+            // setImage2(img2);
+            // setImage3(img3);
+            // setVideoPath(video);
+            // setDescription(desc);
             // console.log("Response: ",response)
             // console.log(video)
         } catch (error) {
@@ -46,8 +46,8 @@ const LandingPage = () => {
         <div >
             <div className="min-h-screen bg-cover  bg-gray-200 bg-center overflow-hidden">
                 <VideoBackground videoPath={videoPath} />
-                {(image1 != null && image2 != null && image3 != null) ? <Diamonds image1={image1} image2={image2} image3={image3} /> : <> </>}
-
+                {/* {(image1 != null && image2 != null && image3 != null) ? <Diamonds image1={image1} image2={image2} image3={image3} /> : <> </>} */}
+                <Diamonds />
                 <div className='w-full '>
                     <div className='sm:text-9xl text-3xl  w-3/4 text-left font-bold text-yellow-500 ml-5 sm:mt-12  transform scale-y-150'>
                         <Typewriter
@@ -73,8 +73,7 @@ const LandingPage = () => {
                     </button>
                 </Link>
                 <Link href="/games">
-
-                    <button className="text-center my-4 sm:my-5 mx-10 transform px-8 py-2 bg-slate-900 bg-opacity-50 bg-grad-hover hover:shadow-lg hover:shadow-slate-900 text-white text-2xl rounded-2xl shadow-md sm:h-16 sm:w-72 ">
+                    <button className="text-center my-4 sm:my-5 mx-10 transform px-8 py-2 bg-slate-900 bg-opacity-50 bg-grad-hover hover:shadow-lg hover:shadow-slate-900 text-white text-2xl rounded-2xl shadow-md sm:h-16 sm:w-80 ">
                         Games
                     </button>
                 </Link>
